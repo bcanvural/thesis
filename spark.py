973944ed79cb12b675e2ef1788d4dbabb2eaf3db
Baris Can Vural <bcanvural@gmail.com>
2017-03-24 17:44:53 +0100

    OneVsRest classification

--- spark.py
+++ spark.py
@@ -3,15 +3,16 @@ from pyspark import SparkContext
 from pyspark.ml.feature import HashingTF, IDF, Tokenizer
 from pyspark.mllib.util import Vectors
 from pyspark.sql import Row
-from pyspark.ml.classification import NaiveBayes
+from pyspark.ml.classification import NaiveBayes, OneVsRest
 from pyspark.ml.feature import StringIndexer
 def parseFeatures(jobId, features):
-    return Row(label=int(jobId), features=features)
+    return Row(label=jobId, features=features)
 
 def main() :
     spark = SparkSession.builder \
         .appName("Spark CV-job ad matching") \
         .config("spark.some.config.option", "some-value") \
+        .master("local[4]") \
         .getOrCreate()
 
     df = spark.read.json("alljobs4rdd/alljobs.jsonl")
@@ -32,20 +33,22 @@ def main() :
     idfModel = idf.fit(featurizedData)
     rescaledData = idfModel.transform(featurizedData)
 
-    # rescaledData.select("jobId", "features").rdd.map(lambda x: (x[0], Vectors.stringify(x[1]))).saveAsTextFile("out")
+    #rescaledData.select("jobId", "features").rdd.map(lambda x: (x[0], Vectors.stringify(x[1]))).saveAsTextFile("out")
+
     #TF-IDF featurization END
 
-
-    rowDF = rescaledData.select("indexed", "features").rdd.map(lambda x: parseFeatures(x.jobId, x.features)).toDF()
     #Naive-Bayes Classifier START
-    indexer =  StringIndexer(inputCol="jobId", outputCol="indexed")
-    indexer_model = indexer.fit(rescaledData)
-    indexed = indexer_model.transform(rescaledData)
+    rowDF = rescaledData.rdd.map(lambda x: parseFeatures(x.jobId, x.features)).toDF()
 
-    rowDF = indexed.select("indexed", "features").rdd.map(lambda x: parseFeatures(x.jobId, x.features)).toDF()
+    # indexer =  StringIndexer(inputCol="label", outputCol="indexed")
+    # indexer_model = indexer.fit(rowDF)
+    # indexed = indexer_model.transform(rowDF)
+
+    # indexed.select("indexed", "features").rdd.map(lambda x: (x[0], Vectors.stringify(x[1]))).saveAsTextFile("out")
+
     nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
-    nb.numClasses = 1007
-    model = nb.fit(rowDF)
+    ovr = OneVsRest(classifier=nb)
+    ovr_model = ovr.fit(rowDF)
 
     #Naive-Bayes Classifier END
 
