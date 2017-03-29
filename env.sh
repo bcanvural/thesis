adaada872c6d436545e6dbebccef75ec0a26cbff
bcanvural <bcanvural@gmail.com>
2017-03-29 20:48:19 +0200

    Train OneVsRestNaiveBayes model

--- env.sh
+++ env.sh
@@ -2,5 +2,4 @@
 
 export PATH=$PATH:/home/baris/Developer/thesis/spark-2.1.0-bin-hadoop2.7/bin
 export PYSPARK_PYTHON=python3
-alias jobss="cat jobs4rdd/* > alljobs4rdd/alljobs.jsonl"
 alias rr="spark-submit --packages com.databricks:spark-csv_2.11:1.5.0 spark.py"
--- spark.py
+++ spark.py
@@ -3,20 +3,22 @@ from pyspark import SparkContext
 from pyspark.ml.feature import HashingTF, IDF, Tokenizer
 from pyspark.mllib.util import Vectors
 from pyspark.sql import Row
-from pyspark.ml.classification import NaiveBayes, OneVsRest
+from pyspark.ml.classification import NaiveBayes, OneVsRest, OneVsRestModel
 from pyspark.ml.feature import StringIndexer
 def parseFeatures(jobId, features):
     return Row(label=jobId, features=features)
+def parseFeaturesCV(features):
+    return Row(features=features)
 
 def main() :
     spark = SparkSession.builder \
         .appName("Spark CV-job ad matching") \
         .config("spark.some.config.option", "some-value") \
-        .master("local[4]") \
+        .master("local[*]") \
         .getOrCreate()
 
     df_jobs = spark.read.json("alljobs4rdd/alljobs.jsonl")
-    df_cvs = spark.read.json("allcvs4rdd/allcvs.jsonl")
+
     filtered = df_jobs.filter("description is not NULL")
 
     # filtered.select("jobId","description").write.format("com.databricks.spark.csv").save("out")
@@ -28,31 +30,52 @@ def main() :
     #numfeatures should be an exponent of 2
     hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=32)
     featurizedData = hashingTF.transform(wordsData)
-    # alternatively, CountVectorizer can also be used to get term frequency vectors
 
     idf = IDF(inputCol="rawFeatures", outputCol="features")
     idfModel = idf.fit(featurizedData)
     rescaledData = idfModel.transform(featurizedData)
 
-    #rescaledData.select("jobId", "features").rdd.map(lambda x: (x[0], Vectors.stringify(x[1]))).saveAsTextFile("out")
+    rescaledData.select("jobId", "features").rdd.map(lambda x: (x[0], Vectors.stringify(x[1]))).saveAsTextFile("out")
 
     #TF-IDF featurization END
 
     #Naive-Bayes Classifier START
-    rowDF = rescaledData.rdd.map(lambda x: parseFeatures(x.jobId, x.features)).toDF()
-    rowDF.rdd.saveAsTextFile("out")
+    rowDF = rescaledData.rdd.map(lambda x: parseFeatures(x.jobId, x.features)).toDF().cache()
+    # rowDF.rdd.saveAsTextFile("out")
 
     # indexer =  StringIndexer(inputCol="label", outputCol="indexed")
     # indexer_model = indexer.fit(rowDF)
     # indexed = indexer_model.transform(rowDF)
 
-    # indexed.select("indexed", "features").rdd.map(lambda x: (x[0], Vectors.stringify(x[1]))).saveAsTextFile("out")
+    #indexed.select("indexed", "features").rdd.map(lambda x: (x[0], Vectors.stringify(x[1]))).saveAsTextFile("out")
 
-    # nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
-    # ovr = OneVsRest(classifier=nb)
+    nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
+    ovr = OneVsRest(classifier=nb)
     # ovr_model = ovr.fit(rowDF)
+    #ovr_model.save("OneVsRestNaiveBayes.model")
+    ovr_model_loaded = OneVsRestModel.load("OneVsRestNaiveBayes")
 
     #Naive-Bayes Classifier END
+
+
+    #Process CVs START
+    df_cvs = spark.read.json("allcvs4rdd/allcvs.jsonl")
+    tokenizer_cvs = Tokenizer(inputCol="description", outputCol="words")
+    wordsData_cvs = tokenizer_cvs.transform(df_cvs)
+
+    #numfeatures should be an exponent of 2
+    hashingTF_cvs = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=32)
+    featurizedData_cvs = hashingTF_cvs.transform(wordsData_cvs)
+
+    idf_cvs = IDF(inputCol="rawFeatures", outputCol="features")
+    idfModel_cvs = idf_cvs.fit(featurizedData_cvs)
+    rescaledData_cvs = idfModel_cvs.transform(featurizedData_cvs)
+    # rescaledData_cvs.select("cvid", "features").rdd.map(lambda x: (x[0], Vectors.stringify(x[1]))).saveAsTextFile("outcv")
+
+    ovr_model_loaded.transform(rescaledData_cvs.rdd.map(lambda x: parseFeaturesCV(x.features)).toDF())\
+    .rdd.map(lambda x: (x[0], Vectors.stringify(x[1])))\
+    .saveAsTextFile("outresults")
+    #Process CVs END
 
 
 
