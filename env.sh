1de54263dcdabc22c260128fa626616e3cd79eb2
bcanvural <bcanvural@gmail.com>
2017-04-10 18:05:46 +0200

    Update aliases

--- env.sh
+++ env.sh
@@ -2,11 +2,12 @@
 
 export PATH=$PATH:/home/baris/Developer/thesis/spark-2.1.0-bin-hadoop2.7/bin
 export PYSPARK_PYTHON=python3
+alias ccall="rm -rf Calculated"
 alias rr1="spark-submit tfidf.py"
-alias cc1="rm -rf cosine-calculated category-cosine-calculated"
+alias cc1="rm -rf Calculated/tfidf Calculated/tfidf"
 alias rr2="spark-submit tfidf2.py"
-alias cc2="rm -rf cosine-calculated-tfidf2 category-cosine-calculated-tfidf2"
+alias cc2="rm -rf Calculated/tfidf2 Calculated/tfidf2"
 alias rr3="spark-submit countvectorizer.py"
-alias cc3="rm -rf cosine-calculated-countvectorizer category-cosine-calculated-countvectorizer"
+alias cc3="rm -rf Calculated/countvectorizer/ Calculated/countvectorizer"
 alias rr4="spark-submit word2vec.py"
-alias cc4="rm -rf word2vec-calculated category-word2vec-calculated"
+alias cc4="rm -rf Calculated/word2vec Calculated/countvectorizer"
