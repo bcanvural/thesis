0f80f6cc4bc9c2464f915cb972952381235b07cf
bcanvural <bcanvural@gmail.com>
2017-04-11 15:34:13 +0200

    Add get cat by name

--- server.py
+++ server.py
@@ -7,13 +7,26 @@ HOST = "127.0.0.1"
 PORT = 5000
 
 @app.route('/job/<id>', methods=['GET'])
-def job_handler(id):
+def job_by_id(id):
     if request.method == 'GET':
         cmd = 'grep \'"jobId": ' + id + '[,}]\' alljobs4rdd/alljobs.jsonl'
         try:
             output = subprocess.check_output(cmd, shell=True).decode("utf-8")
             if output:
-                return jsonify({"respoonse": json.loads(output),"statusCode": 200})
+                return jsonify({"response": json.loads(output),"statusCode": 200})
+            else:
+                return jsonify({"response": {}, "statusCode": 404})
+        except:
+            return jsonify({"response": {}, "statusCode": 404})
+
+@app.route('/category/<name>', methods=['GET'])
+def cat_by_name(name):
+    if request.method == 'GET':
+        cmd = 'grep \'"skillName": "' + name + '"[,}]\' allcategories4rdd/allcategories.jsonl'
+        try:
+            output = subprocess.check_output(cmd, shell=True).decode("utf-8")
+            if output:
+                return jsonify({"response": json.loads(output),"statusCode": 200})
             else:
                 return jsonify({"response": {}, "statusCode": 404})
         except:
