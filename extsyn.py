5d6d0d894c2e7b1b0d58dc948c4ea1a0d951045e
bcanvural <bcanvural@gmail.com>
2017-04-19 18:55:57 +0200

    Remove punctuation in extsyn

--- extsyn.py
+++ extsyn.py
@@ -1,7 +1,11 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Row
 from pyspark.ml.feature import  Tokenizer, StopWordsRemover, Word2VecModel, CountVectorizer, IDF
 from  pyspark.mllib.linalg import SparseVector, Vectors
 import numpy
+from string import punctuation
+
+def strip_punctuation(arr):
+    return [''.join(c for c in s if c not in punctuation) for s in arr]
 
 def add_indices(vector):
     a = []
@@ -30,9 +34,12 @@ def main():
     remover = StopWordsRemover(inputCol="words", outputCol="filtered")
     removed = remover.transform(tokenized)
 
+    stripped = removed.select('jobid', 'filtered').rdd.map(lambda x: (x[0], strip_punctuation(x[1])))\
+    .map(lambda x: Row(jobid=x[1], filtered=x[0])).toDF(['jobid', 'filtered']) #dont know why indices flipped
+
     countVectorizer = CountVectorizer(inputCol="filtered", outputCol="rawFeatures", vocabSize=100, minDF=2.0, binary=False)
-    cv_model = countVectorizer.fit(removed)
-    featurizedData = cv_model.transform(removed)
+    cv_model = countVectorizer.fit(stripped)
+    featurizedData = cv_model.transform(stripped)
 
     idf = IDF(inputCol="rawFeatures", outputCol="features")
     idfModel = idf.fit(featurizedData)
@@ -40,15 +47,12 @@ def main():
     vocab = cv_model.vocabulary
     model = Word2VecModel.load("word2vec-model")
 
-    indexed = rescaledData.select('jobId', 'features')\
-    .rdd.map(lambda row: (row.jobId, sorted(add_indices(row.features), key=lambda x: x[0], reverse=True)))
+    indexed = rescaledData.select('jobid', 'features')\
+    .rdd.map(lambda row: (row.jobid, sorted(add_indices(row.features), key=lambda x: x[0], reverse=True)))
 
     ordered_words = indexed.map(lambda row: (row[0], tuples2words(row[1], vocab)))
     final = ordered_words.map(lambda row: (row[0], " ".join(row[1]))).toDF(['jobid', 'words'])
     final.write.csv('extsyn-importantwords')
-
-
-
 
     # synonyms = model.findSynonyms(sys.argv[1], 10)
     # synonyms.show(truncate=False)
